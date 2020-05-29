2ed901acd04cf8f9426877f7513f4682705ce500
fearless-command-2358 <iuriseara@gmail.com>
2020-05-29 20:20:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array, element)
   array.concat(element)
 end
 
-def using_insert(array, integer, string)
-  array.insert(integer, string)
+def using_insert(array, string)
+  array.insert(string)
 end
 
 def using_uniq(array)
